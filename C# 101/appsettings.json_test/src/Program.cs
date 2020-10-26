1d5a73420827442e2b090dff185c66e6a94ec44a
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2020-10-26 12:03:13 +0300

    appsettings.json introduction

---
+++ C# 101/appsettings.json_test/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+
+            Console.WriteLine($" Hello { config["name"] } !");
+        }
+    }
+}
