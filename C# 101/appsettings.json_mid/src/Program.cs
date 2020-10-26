1b6188342c7a330ed5b4a5b4f943d92ce454c941
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2020-10-26 12:58:33 +0300

    appsettings.json mid lev

---
+++ C# 101/appsettings.json_mid/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+//using FizzBuzz.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Binder;
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You could either use this 
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Or this approach to get your configuration
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            // Then get your values using this approach
+            var appConfig = builder.GetSection("application").Get<Application>(); ;
+
+            Console.WriteLine($"Application Name : {appConfig.Name}");
+        }
+    }
+}
